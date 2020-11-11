1ba11c1d35265a1870dbcaf7989a158746a21898
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-11 11:11:50 -0500

    compare outputs

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,14 +4,15 @@ const util = require('util')
 describe('Universal code', () => {
   it('runs in the browser', function () {
     const sum = require('raw-loader?esModule=false!../../src/sum')
-    let log = ''
+    let browserOutput = ''
     cy.stub(console, 'log').callsFake(function (...args) {
-      log += util.format.apply(util, args)
+      browserOutput += util.format.apply(util, args)
     })
     window.eval(sum)
-    // now assert the log has the expected test
-    expect(log, 'console.log').to.equal('2 + 3 = 5')
 
     cy.task('node', '../../src/sum')
+      .then(nodeOutput => {
+        expect(nodeOutput).to.equal(browserOutput)
+      })
   })
 })
