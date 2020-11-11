53c8d43f8539cdb70d064f1daa57127cda4b6613
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-11 10:42:06 -0500

    working eval

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
-require('../../sum')
 
 describe('Universal code', () => {
   it('runs in the browser', function () {
+    const sum = require('raw-loader?esModule=false!../../src/sum')
+    window.eval(sum)
   })
 })
