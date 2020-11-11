7201368ed10112ca4d2b575dfdf5f8a4d7309762
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-11 11:06:53 -0500

    run in Node

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,8 +1,17 @@
 /// <reference types="cypress" />
+const util = require('util')
 
 describe('Universal code', () => {
   it('runs in the browser', function () {
     const sum = require('raw-loader?esModule=false!../../src/sum')
+    let log = ''
+    cy.stub(console, 'log').callsFake(function (...args) {
+      log += util.format.apply(util, args)
+    })
     window.eval(sum)
+    // now assert the log has the expected test
+    expect(log, 'console.log').to.equal('2 + 3 = 5')
+
+    cy.task('node', '../../src/sum')
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,19 @@
-/// <reference types="cypress" />
-
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
+const util = require('util')
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('task', {
+    node(filename) {
+      console.log('running %s', filename)
+
+      let text = ''
+      const log = console.log
+      console.log = function (...args) {
+        text += util.format.apply(util, args)
+        log.apply(null, args)
+      }
+
+      require(filename)
+
+      return text
+    }
+  })
 }
